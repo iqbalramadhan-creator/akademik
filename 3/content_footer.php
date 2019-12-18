9d5e1f660ff3a0e79dee75bf1010ff1d703f2fac
iqbalramadhan-creator <iqbalramadhan73@gmail.com>
2019-12-18 17:17:56 +0700

    1 aplod

---
+++ 3/content_footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs"></div>
+    <strong>Copyright &copy; <?php echo date("Y") ?> Sistem Akademik UAD and Created By ALBAL (Alif & Iqbal)</a></strong>
+</footer>
